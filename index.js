4742302450bbf4eda419f787154b6b84bc307a4f
michaelfan0310 <michaelfan0310@gmail.com>
2021-09-25 02:18:47 -0400

    Salmon Objection and function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // Document.querySelector(".jellyfish3").classList.toggle("invisible");
 var sketchProc = function (processingInstance) {
     with (processingInstance) {
-        size(2020, 1180);
+        size(1920, 1080);
         frameRate(30);
         var canvas = document.querySelector('canvas');
         var c = canvas.getContext('2d');
@@ -21,18 +21,9 @@ var sketchProc = function (processingInstance) {
         var centerX = 80;
         var centerY = 60;
         var bodyLength = 63;
-        var bodyHeight = 37;
-        var centX = 150;
-        var centY = 138;
-        var bodyL = 84;
-        var bodyH = 49;
-        var X = 50;
-        var Y = 1100;
+        var bodyHeight = 37;      
 
-        var centerx = 900;
-        centx = 880;
-        var centery = 230;
-        centy = 360;
+        
 
         var Particle = function (position) {
             this.acceleration = new PVector(0, -0.05);
@@ -110,12 +101,15 @@ var sketchProc = function (processingInstance) {
 
         Fish.prototype.display = function () {
             noStroke();
-            fill(255, (this.position.x / 3), (this.position.y / 3));
-            ellipse(this.position.x, this.position.y, this.width, this.height);
+            fill(235, (this.position.x / 5), (this.position.y / 4));
             triangle(this.position.x - this.width / 2 + 10, this.position.y,
                 this.position.x - this.width * 0.75, this.position.y + this.height / 3,
                 this.position.x - this.width * 0.75, this.position.y - this.height / 3);
-            fill(235, (this.position.x / 5), (this.position.y / 4));
+            fill(255, (this.position.x / 3), (this.position.y / 3));
+            ellipse(this.position.x, this.position.y, this.width, this.height);
+           
+            fill(235, (this.position.x/ 5), (this.position.y / 4));
+            
             triangle(this.position.x + this.width / 2 + 10, this.position.y - this.height / 12,
                 this.position.x + this.width / 2 - 2, this.position.y + this.height / 8,
                 this.position.x + this.width / 2 - 2, this.position.y - this.height / 8);
@@ -141,9 +135,58 @@ var sketchProc = function (processingInstance) {
         var fish3 = new Fish(new PVector(width/8, height/8));
         var bubbles3 = new ParticleSystem(fish3.getMouthPosition());
 
+        function Salmon(centerX, centerY, bodyLength, bodyHeight) {
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.bodyLength = bodyLength;
+            this.bodyHeight = bodyHeight;
+        };
+        // var EyeX= (this.centerX + this.bodyLength * 0.3);
+        // var EyeY= (this.centerY - this.bodyHeight * 0.4);
+
+        Salmon.prototype.display = function() {
+            noStroke();
+            // fill(233, 35, 35);
+            fill((this.centerX + this.centerY), (this.centerX / 10), (this.cenertY / 10));
+            ellipse(this.centerX, this.centerY, this.bodyLength, this.bodyHeight);
+            fill(centerX, 129, centerY);
+            triangle(this.centerX - this.bodyLength / 2, this.centerY, this.centerX - this.bodyLength / 2 - this.bodyLength / 4, this.centerY - this.bodyHeight / 2, this.centerX - this.bodyLength / 2 - this.bodyLength / 4, this.centerY + this.bodyHeight / 2);
+
+            // eye
+            fill(33, 33, 33);
+            ellipse((this.centerX + this.bodyLength * 0.3), (this.centerY - this.bodyHeight * 0.1), this.bodyHeight / 5, this.bodyHeight / 5);
+        }
+        Salmon.prototype.swim = function () {
+            this.centerX += 3;
+            this.centerY += 0.8;
+
+            if (this.centerX > 1950) {
+                this.centerX = 0;
+            }
+            if (this.centerY > 1090) {
+                this.centerY = 30;
+            }
+        }
+        var salmon = new Salmon(60, 45, 65, 38);
+        var salmon1 = new Salmon(100, 160, 50, 29);
+        var salmon2 = new Salmon(150, 65, 65, 38);
+      for (var i = 3; i < 18; i++) {
+        var salmon_i = new Salmon(60 * (i - 1), 45, 65, 38);
+        }
+
 
         draw = function () {
             background(69, 176, 255, 25);
+
+
+            salmon.display();
+            salmon.swim();
+            salmon1.display();
+            salmon1.swim();
+            salmon2.display();
+            salmon2.swim();
+            salmon_i.display();
+            salmon_i.swim();
 
             if (frameCount % 17 === 1) {
                 bubbles.addParticle();
@@ -168,172 +211,8 @@ var sketchProc = function (processingInstance) {
             fish3.swim();
             fish3.display();
 
-            var bodyColor = color(centerX / 4 + 85, centerY / 3 + 155, centerX / 3 + 60);
-
-            noStroke();
-            fill(bodyColor);
-
-            ellipse(centerX, centerY, bodyLength, bodyHeight); // =bodyF1
-            fill(centx / 3, centerX / 3, centY / 4);
-            ellipse(centX, centY, bodyL, bodyH);
-            fill(centx / 3, centy / 5, centY / 4);
-            ellipse(centX, centerY, bodyLength, bodyHeight);
-
-            fill(centerx / 3, centX / 5, centY / 5);
-            ellipse(centerx, centery, bodyLength, bodyHeight); // =bodyF3++
-            ellipse(centx, centy, bodyL, bodyH);
-            fill(centx / 3, centX / 5, centY / 5);
-            ellipse(centx, centery, bodyLength, bodyHeight);
-
-            var tailWidth = bodyLength / 4;
-            var tailHeight = bodyHeight / 2;
-            var tailW = bodyL / 4;
-            var tailH = bodyH / 2;
-
-            fill(centerX, 129, centerY);
-            //<!--other 3 fishes!!>
-
-
-            var tailWidth = bodyLength / 4;
-            var tailHeight = bodyHeight / 2;
-            var tailW = bodyL / 4;
-            var tailH = bodyH / 2;
-            triangle(centerX - bodyLength / 2, centerY,
-                centerX - bodyLength / 2 - tailWidth, centerY - tailHeight,
-                centerX - bodyLength / 2 - tailWidth, centerY + tailHeight); //=tail
-
-
-            triangle(centX - bodyL / 2, centY,
-                centX - bodyL / 2 - tailW, centY - tailH,
-                centX - bodyL / 2 - tailW, centY + tailH); //=tail  
-            triangle(centX - bodyLength / 2, centerY,
-                centX - bodyLength / 2 - tailWidth, centerY - tailHeight,
-                centX - bodyLength / 2 - tailWidth, centerY + tailHeight);
-
-            triangle(centerx - bodyLength / 2, centery,
-                centerx - bodyLength / 2 - tailWidth, centery - tailHeight,
-                centerx - bodyLength / 2 - tailWidth, centery + tailHeight); //=tail
-
-            triangle(centx - bodyL / 2, centy,
-                centx - bodyL / 2 - tailW, centy - tailH,
-                centx - bodyL / 2 - tailW, centy + tailH); //=tail  
-            triangle(centx - bodyLength / 2, centery,
-                centx - bodyLength / 2 - tailWidth, centery - tailHeight,
-                centx - bodyLength / 2 - tailWidth, centery + tailHeight); //other 3 here
-            // eye
-            fill(33, 33, 33);
-            ellipse(centerX + bodyL / 4, centerY, bodyHeight / 5, bodyHeight / 5);
-
-
-            ellipse(centX + bodyL / 4, centY, bodyH / 5, bodyH / 5);
-
-
-
-            ellipse(centX + bodyL / 4, centerY, bodyHeight / 5, bodyHeight / 5);
-
-
-            ellipse(centerx + bodyL / 4, centery, bodyH / 5, bodyH / 5);
-
-            // eye
-
-
-            ellipse(centx + bodyLength / 4, centy, bodyHeight / 5, bodyHeight / 5);
-
-
-            ellipse(centx + bodyLength / 4, centery, bodyHeight / 5, bodyHeight / 5);
-
-
-            fill(245, 208, 208);
-            ellipse(centerX + bodyLength / 4, centerY, bodyHeight / 19, bodyHeight / 15);
-            rect(centX + bodyL / 4, centY, bodyH / 25, bodyH / 25);
-
-            ellipse(centerx + bodyLength / 4, centery, bodyHeight / 19, bodyHeight / 15);
-            rect(centx + bodyL / 4, centy, bodyH / 25, bodyH / 25);
-
-
-            centerX += 3.8;
-            centX += 2.9;
-            centerY += 0.8;
-            centY += 1.2;
-
-            centerx += 4.5;
-            centx += 5.8;
-            centery += 1.5;
-            centy += 1.9;
-            if (centerX > 1950) {
-                centerX = 0;
-            }
-            if (centX > 1950) {
-                centX = 0;
-            }
-            if (centerY > 1080) {
-                centerY = 30;
-            }
-            if (centY > 1080) {
-                centY = 30;
-            }
-
-            if (centerx > 1900) {
-                centerx = 50;
-            } //for another3
-            if (centx > 1900) {
-                centx = 80;
-            }
-            if (centery > 1080) {
-                centery = 130;
-            }
-            if (centy > 1080) {
-                centy = 230;
-            }
-
-
-
-            stroke(212, 217, 222);
-            noFill();
-            ellipse(430, Y * 2, 10, 10);
-            Y -= 1.3;
-            X += 0.3;
-            ellipse(365, Y * 3, 10, 10);
-            ellipse(Y * 16, X, 11, 11);
-            ellipse(X * 19, 80, 8, 7);
-            ellipse(820, Y + 30, 10, 10);
-            Y -= 1;
-            X += 0.5;
-            ellipse(65, Y - 20, 10, 10);
-            ellipse(Y, X, 11, 11);
-            ellipse(530, Y, 8, 7);
-            ellipse(X * 20, Y, 10, 10);
-            Y -= 2;
-            X += 0.3;
-            ellipse(65, Y, 10, 10);
-            ellipse(Y + X, X, 11, 11);
-            ellipse(X * 30, 80, 8, 7);
-            ellipse(900, Y * 1.2, 10, 10);
-            Y -= 1.5;
-            X += 0.6;
-            ellipse(650, Y * 4, 10, 10);
-            ellipse(700, X, 11, 11);
-            ellipse(X, 590, 8, 7);
-            ellipse(700, Y + 80, 10, 10);
-            Y -= 0.9;
-            X += 1.3;
-            ellipse(465, Y * 1.5, 10, 10);
-            ellipse(830, X * 2, 11, 11);
-            ellipse(910, Y * 1.1, 8, 7);
-
-            if (Y < 0) {
-                Y = 1100;
-            } // for other 3
-            if (X > 1500) {
-                X = 50;
-            }
-        };
-
-
-
-
-
-    }
+    };
+  }
 };
 
 
